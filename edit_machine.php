b07f6e7281a69a58058f16046e30786a9d716fe5
MichaelvMorrison <morrim5@rpi.edu>
2019-03-11 11:03:53 -0400

    Added correct border focus colors for the page

--- edit_machine.php
+++ edit_machine.php
@@ -24,12 +24,12 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div class="card shadow-lg mt-5">
+            <div class="card shadow-lg mt-5 mb-3">
                 <div class="card-body">
                     <h1 class="card-title text-center">Machine Lookup</h1>
                     <form id="get_info">
                         <div class="form-group">
-                            <select class="custom-select" name="machine" id="machine" required>
+                            <select class="custom-select green-focus-border" name="machine" id="machine" required>
                                 <option disabled selected class="text-secondary">Select A Machine to Edit</option>
                                 <?php generateTotalMachineDropDown()?>
                             </select>
@@ -55,12 +55,12 @@
                     <h1 class="card-title text-center">Add A Machine</h1>
                     <form action="controllers/add_hardware_controller.php" method="post">
                         <div class="form-group">
-                            <input type="text" id="addMachineName" class="form-control" name="addMachineName" placeholder="Machine Name" required/>
+                            <input type="text" id="addMachineName" class="form-control purple-focus-border" name="addMachineName" placeholder="Machine Name" required/>
                         </div>
 
                         <div class="form-group">
                             <label>Filament Status: </label>
-                            <select class="custom-select" name="usesPlastic" id="usesPlastic" required>
+                            <select class="custom-select purple-focus-border" name="usesPlastic" id="usesPlastic" required>
                                 <option disabled selected class="text-secondary" id="defaultStatus">Does This Machine Use Filament?</option>
                                 <option value='1'>Yes</option>
                                 <option value = '0'>No</option>
@@ -70,7 +70,7 @@
                         <div id="AddMachineMaterialsWrapper" class="d-none">
                             <div class="form-group">
                                 <label>Multiple Extrusion Status (Does This Machine Use More Than One Filament/Extruder):</label>
-                                <select class="custom-select" name="addMaterials" id="addMaterials">
+                                <select class="custom-select purple-focus-border" name="addMaterials" id="addMaterials">
                                     <option disabled selected class="text-secondary" id="add_mult_ext" value="null">Multiple Extrusion Status</option>
                                     <option value="1" id="mult_ext_true">Yes</option>
                                     <option value="0" id="mult_ext_false">No</option>
@@ -79,12 +79,12 @@
 
                             <div class="form-group d-none" id="add_num_ext">
                                 <label>Max Number of Extrusions Supported:</label>
-                                <input type="number" id="add_num_ext_val" value="1" min="1" max="99" class="form-control" name="add_num_ext" placeholder="Max Number of Extrusions Supported"/>
+                                <input type="number" id="add_num_ext_val" value="1" min="1" max="99" class="form-control purple-focus-border" name="add_num_ext" placeholder="Max Number of Extrusions Supported"/>
                             </div>
                         </div>
 
-                        <div class="form-group text-center">
-                            <button class="btn btn-success btn-clock text-uppercase" type="submit" name="submit">Submit</button>
+                        <div class="text-center">
+                            <button class="btn btn-success btn-clock text-uppercase text-center" type="submit" style="background-color: #6f42c1" name="submit">Add Machine</button>
                         </div>
                     </form>
                 </div>
@@ -132,7 +132,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group text-center">
+                        <div class="text-center">
                             <button class="btn btn-primary btn-clock text-uppercase" type="submit" name="submit">Save</button>
                         </div>
                     </form>
