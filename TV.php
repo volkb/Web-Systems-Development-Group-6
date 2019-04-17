866a20f9a8b263e8867b8b1cffe41d167c3fc6ad
Michael Morrison <morrim5@rpi.edu>
2019-04-17 16:58:40 -0400

    status bars for tv page

---
+++ TV.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="bg-secondary" lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Machines</title>
+  <?php include 'style.php';?>
+  <link rel="stylesheet" type="text/css" href="css/loading-bar.css"/>
+</head>
+
+<body class="bg-secondary">
+  <div class="container-fluid" id="statuses">
+  </div>
+</body>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/TV.js"></script>
+<script type="text/javascript" src="js/loading-bar.js"></script>
+</html>
