9c6e4cc5179bfd9899a93d2aef6dfe26239687cd
Samspre <samantha@sprecace.com>
2019-09-11 17:58:13 -0400

    Added Hover Effects to cards on equipment.php (bootstrap.css and style.css) Added new images to equipment.php TODO: rotate images

--- Equipment.php
+++ Equipment.php
@@ -6,9 +6,14 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!--  <link rel="stylesheet" href="css/mdb.lite.min.css">-->
   <title>Equipment</title>
 </head>
 <body class="bg-light">
+<!--  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>-->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/popper.min.js"></script>
   <div class="bg-light pt-3 p-2">
       <?php include 'nav_bar.php';?>
   </div>
@@ -27,67 +32,146 @@
     <div class="row">
       <div class="col-md-3">
         <h2 class="text-primary">3D Printers</h2>
-        <p>We offer a variety of 3D printing options. Each with their own pros and cons. &nbsp;PLA, ABS, SLA, and even flexible filament.</p>
+        <p>Here at The Forge our 3D Printers are capable of utilizing a wide variety of filament types including: PLA, PETG, ABS, TPU, and PVA. Hover over each printer to find out more information about it!</p>
       </div>
-
-<!--      TODO: z18 and formlabs need landscape photos-->
 
       <div class="container col-md-9">
         <div class="align-self-center mx-1">
           <div class="row">
             <div class="col-md-4 p-0">
+
               <div class="card">
-<!--                <img class="card-img" src="images/Z18A.JPG" alt="Card image" />-->
-                <img class="card-img" src="homePagePhotos/z18.jpg" alt="Card image" />
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h2 class="text-secondary">Makerbot Z18</h2>
+                <div class="view overlay">
+                  <img class="card-img-top" src="images/formlabs.jpg" alt="Card image cap">
+                  <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                    <h2 class="text-secondary card-text">Formlabs 1+</h2>
+                    <div class="card-img-overlay mask flex-center rgba-red-strong">
+                      <p class="text-secondary">If accuracy is what you are looking for, this is the machine for you. Formlabs utilizes SLA printing techniques to allow for layer heights as low as 2.5 microns!</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
             <div class="col-md-4 p-0">
               <div class="card">
-                <img class="card-img" src="homePagePhotos/taz5.jpg" alt="Card image">
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h2 class="text-secondary">Tazbot 5</h2>
+                <div class="view overlay">
+                  <img class="card-img-top" src="images/gCreate.JPG" alt="Card image cap">
+                  <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                    <h2 class="text-secondary card-text">gCreate 1.5XT+</h2>
+                    <div class="card-img-overlay mask flex-center rgba-red-strong">
+                      <p class="text-secondary"> The gCreate is the last word in size here in The Forge, if your model is truly massive, or you want it in a material other than PLA, this is the machine for you</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
             <div class="col-md-4 p-0">
               <div class="card">
-                <img class="card-img" src="homePagePhotos/tazMini.jpg" alt="Card image">
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h2 class="text-secondary">Tazbot Mini</h2>
+                <div class="view overlay">
+                  <img class="card-img-top" src="images/lulzbotMini.JPG" alt="Card image cap">
+                  <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                    <h2 class="text-secondary card-text">LulzBot Mini</h2>
+                    <div class="card-img-overlay mask flex-center rgba-red-strong">
+                      <p class="text-secondary">The Lulzbot Mini is small but powerful, capable of printing relatively small objects with high detail, it has been a reliable workhorse here in The Forge since the beginning</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+
+          <div class="row">
+              <div class="col-md-4 p-0">
+                <div class="card">
+                  <div class="view overlay">
+                    <img class="card-img-top" src="images/lulzbot.JPG" alt="Card image cap">
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                      <h2 class="text-secondary text-center card-text">Lulzbot MOAR-Struder</h2>
+                      <div class="card-img-overlay mask flex-center rgba-red-strong">
+                        <p class="text-secondary">This machine utilizes an enlarged nozzle and all metal hotend to lay down as much plastic as fast as possible! as a result, prints off of this machine are slightly less detailed, but print in 1/4 the time!</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            <div class="col-md-4 p-0">
+              <div class="card">
+                <div class="view overlay">
+                  <img class="card-img-top" src="images/quad.JPG" alt="Card image cap">
+                  <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                    <h2 class="text-secondary text-center card-text">Lulzbot Quadfusion</h2>
+                    <div class="card-img-overlay mask flex-center rgba-red-strong">
+                      <p class="text-secondary">The Quadfusion is truly unique, while it prints relatively slow, it is capable of taking 4 spools: Cyan, Magenta, Yellow, and White to make ANY color you want. In short, this machine is capable of full color 3D printing with zero post processing</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 p-0">
+              <div class="card">
+                <div class="view overlay">
+                  <img class="card-img-top" src="images/mini.JPG" alt="Card image cap">
+                  <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                    <h2 class="text-secondary text-center card-text">Makerbot Mini</h2>
+                    <div class="card-img-overlay mask flex-center rgba-red-strong">
+                      <p class="text-secondary">The Makerbot Mini is often available to use due to its relatively small buildplate, however this is often useful for anyone who is interested in printing small pieces such as minatures!</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
           <div class="row">
             <div class="col-md-4 p-0">
               <div class="card">
-                <img class="card-img" src="homePagePhotos/flexyTaz.jpg" alt="Card image">
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h2 class="text-secondary">Tazbot Flexy</h2>
+                <div class="view overlay">
+                  <img class="card-img-top" src="images/Z18A.JPG" alt="Card image cap">
+                  <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                    <h2 class="text-secondary text-center card-text">Makerbot Z18</h2>
+                    <div class="card-img-overlay mask flex-center rgba-red-strong">
+                      <p class="text-secondary">The Z18s are capable of printing large models in PLA, and are well suited for anything that is too big to fit on a standard buildplate</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
             <div class="col-md-4 p-0">
               <div class="card">
-<!--                <img class="card-img" src="images/Z18A.JPG" alt="Card image" />-->
-                <img class="card-img" src="homePagePhotos/formlabs.jpg" alt="Card image" />
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h2 class="text-secondary">Formlabs</h2>
+                <div class="view overlay">
+                  <img class="card-img-top" src="images/markforged.JPG" alt="Card image cap">
+                  <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                    <h2 class="text-secondary text-center card-text">Markforged Onyx Pro</h2>
+                    <div class="card-img-overlay mask flex-center rgba-red-strong">
+                      <p class="text-secondary">If your print needs to be very strong, this is the machine to use! This machine uses chopped carbon and fiberglass to create models that are 20x stronger than standard ABS printing!</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
             <div class="col-md-4 p-0">
               <div class="card">
-                <img class="card-img" src="images/mini.JPG" alt="Card image">
-                <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                  <h2 class="text-secondary">Makerbot Mini</h2>
+                <div class="view overlay">
+                  <img class="card-img-top" src="images/prusa.JPG" alt="Card image cap">
+                  <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                    <h2 class="text-secondary text-center card-text">Prusa i3Mk3S</h2>
+                    <div class="card-img-overlay mask flex-center rgba-red-strong">
+                      <p class="text-secondary">Here in The Forge, our Prusa's are the workhorses, capable of high speed, high accuracy printing with minimal downtime, if your model is able to be broken in to multiple parts, or not too large, these are the go to machines!</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
           </div>
+
         </div>
       </div>
 
@@ -99,7 +183,7 @@
       <div class="row">
         <div class="col-md-7">
           <h2 class="text-primary">Laser Cutter</h2>
-          <p class="">We can laser cut or engrave anything short of metal and excluding materials creating known carcinogens (PVC and MDF). &nbsp;Our software takes ".dxf" files but volunteers can also help you engrave or cut from any image file</p>
+          <p class="">One of the most capable machines on campus! This machine can cut any non-metal material, and can engrave anodized metals! With a 2'x3' bed, you can fit most raw materials in as well!</p>
         </div>
         <div class="col-md-5 align-self-center p-0">
           <img class="card-img" src="homePagePhotos/laserCutter.jpg"> </div>
@@ -107,15 +191,19 @@
     </div>
 
   </div>
-  <div class="container card bg-secondary mb-3 py-1 shadow-lg">
-    <div class="align-self-center mx-1">
-      <div class="row">
-        <div class="col-md-7">
-          <h2 class="text-primary">3D Scanner</h2>
-          <p class="">Have an object you want a digital, 3D copy of? If its small enough to fit on our scanner you can get a high quality 3D scan. Just ask one of our volunteers and wear the infared protection glasses.</p>
+    <div class="container card bg-secondary mb-3 py-1 shadow-lg">
+      <div class="align-self-center mx-1">
+        <div class="row">
+          <div class="col-md-4">
+            <h2 class="text-primary">3D Scanner and Electronics Work Station</h2>
+            <p class="">We have two 3D scanners here in The Forge, both are capable of scanning small objects with high detail!</p>
+            <p class=""> Need to do some basic soldering, use an oscilloscope? The Forge keeps a basic Electronics Work-Station to help you! If your project requires more than this we recommend visiting the Embedded Hardware Club!</p>
+          </div>
+          <div class="col-md-4 align-self-center p-0">
+            <img class="card-img" src="homePagePhotos/3dScanner.jpg"> </div>
+          <div class="col-md-4 align-self-center p-0">
+            <img class="card-img" src="images/sewing.JPG"> </div>
         </div>
-        <div class="col-md-5 align-self-center p-0">
-          <img class="card-img" src="homePagePhotos/3dScanner.jpg"> </div>
       </div>
     </div>
 
@@ -125,7 +213,8 @@
       <div class="row">
         <div class="col-md-4">
           <h2 class="text-primary">Vinyl Cutter and Embroidery</h2>
-          <p class="">Our newly added machines include an Expert Pro Vinyl Cutter and a Brother Sewing and Embroidery Machine</p>
+          <p class="">Looking to design a new shirt logo? Or creating vinyl decals for your laptop? You can do it all here! If you come to The Forge and look around, most of our decals were made on our vinyl cutter!</p>
+          <p class="">Need to do some sewing? Or stitch your favorite logo onto a patch of fabric? We keep a small sewing station equipped with various colors of thread!</p>
         </div>
         <div class="col-md-4 align-self-center p-0">
           <img class="card-img" src="images/vinyl.JPG"> </div>
